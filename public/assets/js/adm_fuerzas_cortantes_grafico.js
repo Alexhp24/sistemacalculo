c4da5c60814d82c579254086bff3440402c48231
Tony <taaffaat_1@hotmail.com>
2024-08-28 09:03:59 -0500

    mensaje de error

--- public/assets/js/adm_fuerzas_cortantes_grafico.js
+++ public/assets/js/adm_fuerzas_cortantes_grafico.js
@@ -705,14 +705,19 @@
           }, 500);
         })
         .catch((error) => {
+          const swalTailwind = Swal.mixin({
+            customClass: {
+              confirmButton: "bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded",
+            },
+            buttonsStyling: false
+          });
           console.log(error);
-          Swal.fire({
+          swalTailwind.fire({
             icon: "error",
             html: `
               ${error}
             `,
-            showCloseButton: true,
-            focusConfirm: false,
+            showConfirmButton: true,
           });
         });
     });
